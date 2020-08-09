6258c5e1245d917ccc2c2cf605332448f54a9c9f
Lucas Guimaraes <guimaraes-lucas@outlook.com.br>
2020-08-09 00:30:04 -0300

    feat: add initial routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
+import { RouterModule } from '@angular/router'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +10,25 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component'
 import { PetsComponent } from './pets/pets.component'
 import { PetDetailComponent } from './pets/pet-detail/pet-detail.component'
+
+const ROUTES = RouterModule.forRoot([
+  // {
+  //   path: 'addresses',
+  //   component: AddressesComponent
+  // },
+  // {
+  //   path: 'users',
+  //   component: UsersComponent
+  // },
+  // {
+  //   path: 'kinds',
+  //   component: KindsComponent
+  // },
+  {
+    path: 'pets',
+    component: PetsComponent
+  }
+])
 
 @NgModule({
   declarations: [
@@ -19,7 +40,10 @@ import { PetDetailComponent } from './pets/pet-detail/pet-detail.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    RouterModule,
+    ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
