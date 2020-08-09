2a49df3c370b779f9c5069b83216fefdf018a04c
Lucas Guimaraes <guimaraes-lucas@outlook.com.br>
2020-08-08 23:37:50 -0300

    refactor: add PetService and dependency injection

--- src/app/pets/pets.component.ts
+++ src/app/pets/pets.component.ts
@@ -1,91 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Address } from '../addresses/shared/address.model';
-import { User } from '../users/shared/user.model';
-import { Kind } from '../kinds/shared/kind.model';
 import { Pet } from './shared/pet.model';
-
-const user: User = {
-  id: 1,
-  name: 'Luke',
-  email: 'luke@gmail.com',
-  password: '123456',
-  address: new Address()
-};
-
-const kind: Kind = new Kind(1, 'Dog');
-
-const PETS: Array<Pet> = [
-  {
-    id: 1,
-    name: 'Bob 1',
-    breed: 'Pinscher', 
-    age: 3, 
-    weight: 2.5, 
-    city: 'Cristópolis',
-    kind: kind, 
-    user: user
-  },
-  {
-    id: 2,
-    name: 'Bob 2',
-    breed: 'Pinscher', 
-    age: 3, 
-    weight: 2.5, 
-    city: 'Cristópolis',
-    kind: kind, 
-    user: user
-  },
-  {
-    id: 3,
-    name: 'Bob 3',
-    breed: 'Pinscher', 
-    age: 3, 
-    weight: 2.5, 
-    city: 'Cristópolis',
-    kind: kind, 
-    user: user
-  },
-  {
-    id: 4,
-    name: 'Bob 4',
-    breed: 'Pinscher', 
-    age: 3, 
-    weight: 2.5, 
-    city: 'Cristópolis',
-    kind: kind, 
-    user: user
-  },
-  {
-    id: 5,
-    name: 'Bob 5',
-    breed: 'Pinscher', 
-    age: 3, 
-    weight: 2.5, 
-    city: 'Cristópolis',
-    kind: kind, 
-    user: user
-  },
-];
+import { PetService } from './shared/pet.service';
 
 @Component({
   selector: 'pets',
-  templateUrl: './pets.component.html'
+  templateUrl: './pets.component.html',
+  providers: [ PetService ]
 })
 
 export class PetsComponent implements OnInit{
-  public pets;
+  public pets: Array<Pet>;
   public selectedPet: Pet;
 
-  public constructor(){
-    this.pets = ''
-  }
+  public constructor(private petService: PetService){  }
 
   public ngOnInit(){
-    this.pets = PETS;
+    this.pets = this.petService.getPets();
   }  
 
   public onSelect(pet: Pet): void {
-    this.selectedPet = pet
+    this.selectedPet = pet;
   }
 }
---
+++ src/app/pets/shared/pet.service.ts
@@ -0,0 +1,77 @@
+import { Injectable } from '@angular/core';
+
+import { Address } from '../../addresses/shared/address.model';
+import { User } from '../../users/shared/user.model';
+import { Kind } from '../../kinds/shared/kind.model';
+import { Pet } from './pet.model';
+
+const user: User = {
+  id: 1,
+  name: 'Luke',
+  email: 'luke@gmail.com',
+  password: '123456',
+  address: new Address()
+};
+
+const kind: Kind = new Kind(1, 'Dog');
+
+const PETS: Array<Pet> = [
+  {
+    id: 1,
+    name: 'Bob 1',
+    breed: 'Pinscher', 
+    age: 3, 
+    weight: 2.5, 
+    city: 'Cristópolis',
+    kind: kind, 
+    user: user
+  },
+  {
+    id: 2,
+    name: 'Bob 2',
+    breed: 'Pinscher', 
+    age: 3, 
+    weight: 2.5, 
+    city: 'Cristópolis',
+    kind: kind, 
+    user: user
+  },
+  {
+    id: 3,
+    name: 'Bob 3',
+    breed: 'Pinscher', 
+    age: 3, 
+    weight: 2.5, 
+    city: 'Cristópolis',
+    kind: kind, 
+    user: user
+  },
+  {
+    id: 4,
+    name: 'Bob 4',
+    breed: 'Pinscher', 
+    age: 3, 
+    weight: 2.5, 
+    city: 'Cristópolis',
+    kind: kind, 
+    user: user
+  },
+  {
+    id: 5,
+    name: 'Bob 5',
+    breed: 'Pinscher', 
+    age: 3, 
+    weight: 2.5, 
+    city: 'Cristópolis',
+    kind: kind, 
+    user: user
+  },
+];
+
+@Injectable()
+
+export class PetService{
+  public getPets(): Array<Pet> {
+    return PETS;
+  }
+}
