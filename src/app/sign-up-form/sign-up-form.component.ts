f0e9a2d811c3d515ea76729ec42dc2d2ed1b8324
Lucas Guimaraes <guimaraes-lucas@outlook.com.br>
2020-08-16 15:47:16 -0300

    feat: add validation password

--- src/app/sign-up-form/sign-up-form.component.ts
+++ src/app/sign-up-form/sign-up-form.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core'
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms'
 
-import { USER_VALIDATION } from '../users/shared/user.validation'
+import { SIGN_UP_VALIDATION } from '../users/shared/user.validation'
 import { FormUtils } from 'src/app/shared/form.utils'
 
 @Component({
@@ -14,12 +14,19 @@ export class SignUpFormComponent {
   public formUtils: FormUtils
 
   public constructor(private formBuilder: FormBuilder){
-    this.form = this.formBuilder.group(USER_VALIDATION)
+    this.form = this.formBuilder.group(SIGN_UP_VALIDATION, { validators: this.passwordConfirmationValidator })
     this.formUtils = new FormUtils(this.form)
   }
   
   public signUpUser(){
     console.log("Formulário de SignUp enviado!")
     console.log(this.form.value)
+  }
+
+  public passwordConfirmationValidator(form: FormGroup){
+    if(form.get('password').dirty && form.get('password').value === form.get('passwordConfirmation').value)
+      form.get('passwordConfirmation').setErrors(null)
+    else
+      form.get('passwordConfirmation').setErrors({ 'mismatch': true })
   }
 }
--- src/app/users/shared/user.validation.ts
+++ src/app/users/shared/user.validation.ts
@@ -2,7 +2,7 @@ import { Validators } from '@angular/forms'
 
 export const USER_VALIDATION = {
   name: [null, [Validators.required, 
-                Validators.minLength(7), 
+                Validators.minLength(2), 
                 Validators.maxLength(100)]
         ],
   email: [null, [ Validators.required, 
@@ -14,12 +14,13 @@ export const USER_VALIDATION = {
                     Validators.minLength(8),
                     Validators.maxLength(60)]
             ],
-  passwordConfirmation: [null, [ Validators.required,
-                                  Validators.minLength(8),
-                                  Validators.maxLength(60)]
-                         ],
   address: [null, [ Validators.required, 
                     Validators.minLength(2),
                     Validators.maxLength(255)]
             ]
+}
+
+export const SIGN_UP_VALIDATION = { 
+        ...USER_VALIDATION, 
+        ...{ passwordConfirmation: [null, Validators.required] }
 }
